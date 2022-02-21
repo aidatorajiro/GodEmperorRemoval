818c0a233791eb4a8e22edb63c833bd00833c1f3
aidatorajiro <kawarusosu@zoho.com>
2022-02-21 18:29:50 +0900

    implement counter

--- content.js
+++ content.js
@@ -6,7 +6,12 @@ chrome.storage.sync.get("is_enabled", ({is_enabled}) => {
     if (is_enabled === true) {
         chrome.storage.sync.get("method", ({method}) => {
             if (method === "かんたん") {
-                document.body.innerHTML = document.body.innerHTML.replace(/天皇/g, ()=>(rs(["点","店","展","転","添","篆","典","貼","添","填","貂","恬","ten"]) + rs(["脳","嚢","能","農","膿","悩","濃","応","王","喃","納","know"])))
+                let i = 0;
+                document.body.innerHTML = document.body.innerHTML.replace(/天皇/g, ()=>{i++; return rs(["点","店","展","転","添","篆","典","貼","添","填","貂","恬","ten"]) + rs(["脳","嚢","能","農","膿","悩","濃","応","王","喃","納","know"])})
+                console.log("Total match : " + i);
+                chrome.storage.sync.get("counter", ({counter}) => {
+                    chrome.storage.sync.set({counter: counter + i})
+                })
             }
         })
     }
